6ecbfe74ed701d471874ce4310c9516a301945df
moinkhan07 <moin6k@gmail.com>
2023-09-30 15:25:27 +0530

    Backend folder added

---
+++ Backend/MovieVenture/src/main/java/com/movieventure/MovieVentureApplication.java
@@ -0,0 +1,13 @@
+package com.movieventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieVentureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieVentureApplication.class, args);
+	}
+
+}
---
+++ Backend/MovieVenture/src/test/java/com/movieventure/MovieVentureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieVentureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
