80c45d1a91b73671a8c5c0fe9646b0befccb3d6e
moinkhan07 <moin6k@gmail.com>
2023-09-30 14:20:50 +0530

    Frontend Updated

--- movie-website/src/App.js
+++ movie-website/src/App.js
@@ -1,14 +1,21 @@
-import Navbar from './Components/Navbar';
-import Content from './Components/Content'
 import './index.css';
 import './Style/Navbar.css'
-import './Style/Content.css'
+import './Style/MovieList.css'
+import './Style/Footer.css'
+import './Style/MoviePagination.css'
+import Navbar from './Components/Navbar';
+import Content from './Components/Content'
+import Footer from './Components/Footer';
+import MoviePagination from './Components/MoviePagination';
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Content />
+      <MoviePagination/>
+      <Footer />
     </div>
   );
 }
---
+++ movie-website/src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div id='footer'>
+        <p>How to Download | Report Broken Links | Request Us | DMCA | Contact Us | About Us | Site Disclaimer</p>
+    </div>
+  )
+}
+
+export default Footer
