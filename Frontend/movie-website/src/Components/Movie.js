90d3d6f00894ec553d07a9d431b314bde3798a8c
moinkhan07 <moin6k@gmail.com>
2023-10-05 00:26:48 +0530

    basic changes

--- Frontend/movie-website/src/Components/Movie.js
+++ Frontend/movie-website/src/Components/Movie.js
@@ -4,16 +4,12 @@ import { useParams } from 'react-router-dom';
 
 const Movie = () => {
     const { movieId, currentPage } = useParams();
-  const fileID = "";
 
   return (
     <div id='MovieInfoMain'>
         <Navbar/>
         <h1> Movie ID:{movieId}</h1>
         <h1>Page Number:{currentPage}</h1>
-        {/* <video id='videoTag' controls>
-        <source src={`https://drive.google.com/uc?export=download&id=${fileID}`} type="video/mp4" />
-      </video> */}
     </div>
   );
 };
