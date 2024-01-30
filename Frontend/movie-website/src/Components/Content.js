7d0e35c78d7d54ffb67e3ab4b9616114542c710d
moinkhan07 <moin6k@gmail.com>
2024-01-30 14:59:14 +0530

    updated loading spinner

--- Frontend/movie-website/src/Components/Content.js
+++ Frontend/movie-website/src/Components/Content.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import MoviePagination from './MoviePagination';
 import MovieList from './MovieList';
+import { css } from '@emotion/react';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 const Content = ({ searchQuery, selectedCategory }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const moviesPerPage = 20;
   const [movieData,setMovieData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const onPageChange = (page) => {
     setCurrentPage(page);
@@ -13,11 +16,18 @@ const Content = ({ searchQuery, selectedCategory }) => {
 
   const mainUrl = "https://api.movieventure.xyz/api";
 
-  const getMoviesData = async ()=>{
-    let res = await fetch(`${mainUrl}/movies`);
-    let data = await res.json();
-    setMovieData(data);
-  }
+  const getMoviesData = async () => {
+    try {
+      let res = await fetch(`${mainUrl}/movies`);
+      let data = await res.json();
+      setMovieData(data);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching movies:', error);
+      setLoading(false);
+    }
+  };
+  
   useEffect(()=>{
     getMoviesData();
   },[]);
@@ -26,16 +36,23 @@ const Content = ({ searchQuery, selectedCategory }) => {
     getMoviesByCategory();
   },[selectedCategory])
 
-  const getMoviesByCategory = async ()=>{
-    if(selectedCategory !== "All"){
-    let res = await fetch(`${mainUrl}/movies/category/${selectedCategory}`);
-    let data = await res.json();
-    setMovieData(data);
-    }
-    else{
+  const getMoviesByCategory = async () => {
+    setLoading(true);
+
+    if (selectedCategory !== "All") {
+      try {
+        let res = await fetch(`${mainUrl}/movies/category/${selectedCategory}`);
+        let data = await res.json();
+        setMovieData(data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching movies by category:', error);
+        setLoading(false);
+      }
+    } else {
       getMoviesData();
     }
-  }
+  };
   
   const totalPages = Math.ceil(movieData.length / moviesPerPage);
 
@@ -50,9 +67,16 @@ const Content = ({ searchQuery, selectedCategory }) => {
 
   const filteredMovieData = filterMovies();
 
+const override = css`
+    display: block;
+    margin: 0 auto;
+    border-color: red;
+  `;
+
+
   return (
     <>
-      <div id="contentMain">
+      {/* <div id="contentMain">
         {filteredMovieData.length === 0 ? (
           <p style={{
             fontSize: '20px',
@@ -63,6 +87,38 @@ const Content = ({ searchQuery, selectedCategory }) => {
             left: '50%',
             transform: 'translate(-50%, -50%)'
           }} >Not Such Movie Found!</p>
+        ) : (
+          <MovieList
+            currentPage={currentPage}
+            moviesPerPage={moviesPerPage}
+            movieData={filteredMovieData}
+          />
+        )}
+      </div> */}
+       <div id="contentMain">
+        {loading ? (
+          <div style={{
+            textAlign: 'center',
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            marginTop:'50px'
+          }}>
+            {/* Use the ClipLoader from react-spinners */}
+            <ClipLoader color="#FEA641" loading={loading} css={override} size={50} />
+          </div>
+        ) : filteredMovieData.length === 0 ? (
+          <p style={{
+            fontSize: '20px',
+            color: 'red',
+            textAlign: 'center',
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            marginTop:'50px'
+          }}>No such movie found!</p>
         ) : (
           <MovieList
             currentPage={currentPage}
--- Frontend/movie-website/src/Components/Movie.js
+++ Frontend/movie-website/src/Components/Movie.js
@@ -14,6 +14,7 @@ const Movie = () => {
       setData(data);
     }
     useEffect(() => {
+      window.scrollTo(0, 0);
       getMovieData();
     }, [movieId]);
 
